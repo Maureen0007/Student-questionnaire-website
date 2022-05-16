42c82a08c73307c68ea4d72c29433b377531395c
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-05-16 10:06:27 +0300

    add javascript functions

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,28 @@
-var 
+function result(){
+    var score=0;
+    if (document.getElementById('correct1').checked) 
+    {
+        score=+5
+    }
+    if (document.getElementById('correct2').checked) 
+    {
+        score=+5
+    }
+    if (document.getElementById('correct3').checked) 
+    {
+        score=+5
+    }
+       
+    if (document.getElementById('correct4').checked) 
+    {
+        score=+5
+    }
+    if (document.getElementById('correct5').checked) 
+    {
+        score=+5
+    }
+       
+        
+    
+    
+}
